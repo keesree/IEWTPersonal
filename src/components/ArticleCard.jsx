55fb3f90d1cae083d21dfba39ff97bd759572c54
kee <ksrm1010@gmail.com>
2024-10-28 23:18:59 -0700

    adjusted css of home page

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -6,10 +6,9 @@ import '../css/ArticleCard.css'
 const ArticleCard = () => {
   return (
     <div className="articleCard">
-      <img src="https://t4.ftcdn.net/jpg/05/86/77/59/360_F_586775980_e3C7KDR7qoq8gDs0HAIoAxcN8zBpE7QU.jpg" />
-      <h3>Article title</h3>
-      <p>Article date</p>
-      <p>Article tags</p>
+      <img src="src\assets\testImage.jpg" />
+      <h3>Article Title</h3>
+      <p>Article Information</p>
     </div>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,54 +11,51 @@ const Home = () => {
 
   return (
     <div>
-      <div className="featuredCarousel">
+      <div className="featuredCarousel ">
         <Carousel>
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
+          <Carousel.Item >
+            <img 
+              className="d-block w-100 carouselItems"
               src="https://media.gettyimages.com/id/1367479760/photo/view-over-dunes-with-dune-grass-at-sunset-by-the-sea.jpg?s=612x612&w=gi&k=20&c=klT9qRJyskvf0pbmvsFPsKG9B3E4QhO541Qq1qeq5Bk="
               alt="Pic 1"
             />
             <Carousel.Caption>
-              <h3>Article Title</h3>
+              <h3>Inspiring Stories from Around the World!</h3>
             </Carousel.Caption>
           </Carousel.Item>
           <Carousel.Item>
             <img
-              className="d-block w-100"
+              className="d-block w-100 carouselItems"
               src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmWo0AOpzzT_in89hS2rsMy7d4nTxIoaj45A&s"
               alt="Pic 2"
             />
             <Carousel.Caption>
-              <h3>Article Title</h3>
+              <h3>Spreading Joy One Story at a Time!</h3>
             </Carousel.Caption>
           </Carousel.Item>
           <Carousel.Item>
             <img
-              className="d-block w-100"
+              className="d-block w-100 carouselItems"
               src="https://images.unsplash.com/photo-1470240731273-7821a6eeb6bd?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bWVhZG93fGVufDB8fDB8fHww"
               alt="Pic 3"
             />
 
             <Carousel.Caption>
-              <h3>Article Title</h3>
+              <h3>Your Daily Dose of Positivity!</h3>
             </Carousel.Caption>
           </Carousel.Item>
         </Carousel>
       </div>
+      <h3>Check out the latest articles!</h3>
       <div className="articleContainer">
-        <h3>Check out these articles!</h3>
-        <br />
+        
         <ArticleCard />
         <ArticleCard />
         <ArticleCard />
         <ArticleCard />
         <ArticleCard />
         <ArticleCard />
-        <ArticleCard />
-        <ArticleCard />
-        <ArticleCard />
-        <ArticleCard />
+        
       </div>
     </div>
   );
